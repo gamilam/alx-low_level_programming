bd9b37bdf346e0b06dd7d7148d55360d1c1c7255
gamilam <gamilakb@gmail.com>
2023-09-08 15:05:22 -0700

    c world

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/**
+*main - is the main function
+*
+*Return: always return zero
+*/
+int main(void)
+{
+puts("\"Programming is like building a multilingual puzzle");
+return (0);
+}
---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,12 @@
+#include <stdi.h>
+
+/**
+*main - is the main fuction
+*
+*Return: always return zero 
+*/
+int main(void)
+{
+printf ("with proper grammar, but the outcome is a piece of art");
+return (0);
+}
