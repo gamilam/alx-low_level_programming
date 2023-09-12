d7bc342b38545e67c50c49962b6edaef331c9ecd
gamilam <gamilakb@gmail.com>
2023-09-12 16:57:16 -0700

    task4

---
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+* _isalpha - is the function that check
+* @c: parameter
+* Return: 0 or 1
+*/
+int _isalpha(int c)
+{
+if ((c <= 'a' && c >= 'z') || (c <= 'A' && c >= 'Z'))
+{
+return (1);
+}
+else
+{
+return (0);
+}
+}
