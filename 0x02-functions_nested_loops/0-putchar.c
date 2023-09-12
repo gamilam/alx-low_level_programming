00be1fb90a261a9ffd4315c1bee72d1e77949b22
gamilam <gamilakb@gmail.com>
2023-09-12 15:56:55 -0700

    task0

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,19 +1,20 @@
 #include "main.h"
 
 /**
- * main - Print the word "main"
- *
- * Return: Always 0 (Success)
- */
+* main - Print the word "main"
+*
+* Return: Always 0 (Success)
+*/
 int main(void)
 {
-	int i;
-	char p[] = "_putchar";
-
-	for (i = 0; i < 8; i++)
-	{
-		_putchar(p[i]);
-	}
-	_putchar('\n');
-	return (0);
+_putchar('_');
+_putchar('p');
+_putchar('u');
+_putchar('t');
+_putchar('c');
+_putchar('h');
+_putchar('a');
+_putchar('r');
+_putchar('\n');
+return (0);
 }
