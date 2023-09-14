af52ca7867c8c2b6d779673efee4efa6318c5a32
gamilam <gamilakb@gmail.com>
2023-09-14 16:58:04 -0700

    c fun

---
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+*_isdigit - check if c is upper
+*
+*@c: char to be checked
+*
+* Return: 0 or 1
+*/
+
+int _isupper(int c)
+{
+if (c >= '0' && c <= '9')
+return (1)
+else
+return (0)
+
