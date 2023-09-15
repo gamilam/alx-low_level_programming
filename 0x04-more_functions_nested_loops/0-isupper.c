1bd2613a9a4f8e6bd74bc48df1bdcd8396533b61
gamilam <gamilakb@gmail.com>
2023-09-14 17:11:58 -0700

    c fun

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -5,13 +5,13 @@
 *
 *@c: input for alphabet
 *
-* Return: 1 if its uppercase, 0 if not
+* Return: 0 or 1
 */
 
 int _isupper(int c)
 {
-if (c >= 65 && c <= 90)
-return (1)
+if (c >= 'A' && c <= 'Z')
+return (1);
 else
-return (0)
+return (0);
 }
