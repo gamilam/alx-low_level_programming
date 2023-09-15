8b4d396a761723cbaf563bdb45975008ac233a54
gamilam <gamilakb@gmail.com>
2023-09-14 18:30:53 -0700

    c fun

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -1,7 +1,7 @@
 #include "main.h"
 
 /**
-*print_most_numbers - print num expect 2 and 4 
+*print_most_numbers - print num expect 2 and 4
 *
 * Return: void
 */
@@ -11,7 +11,7 @@ void print_most_numbers(void)
 char C;
 for (C = '0'; C <= '9'; C++)
 {
-if (|(C == '2' || C == '4' ))
+if (!(C == '2' | | C == '4'))
 _putchar(C);
 }
 _putchar('\n');
