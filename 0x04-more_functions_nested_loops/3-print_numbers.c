24ebf68e25e53f22bcc4be8b0112b9bfe3971267
gamilam <gamilakb@gmail.com>
2023-09-14 18:07:36 -0700

    c fun

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+*print_numbers - prints 0-9
+*Return: void
+*/
+
+void print_numbers(void)
+{
+char C;
+for (C = '0'; C <= '9'; C++)
+{
+_putchar(C);
+}
+_putchar('\n');
+}
