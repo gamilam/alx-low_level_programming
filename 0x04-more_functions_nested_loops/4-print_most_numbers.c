bfd25d7e927770b6b54d8bdd9ef8c8015f0b93cc
gamilam <gamilakb@gmail.com>
2023-09-15 17:28:04 -0700

    jfggdfds

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -3,22 +3,17 @@
 /**
 *print_most_numbers - print the numbers since 0 up to 9
 *Description: print numbers excluding 2 and 4
-* Return: the numbers since 0 up to 9
+* Return: always = '0'
 */
 
 void print_most_numbers(void)
 {
-int x = 0;
-for (; x <= 9; x++)
+int num;
+for (num = 0; num <= 9; num++)
 {
-if (x == 2 || x == 4)
-{
+if (num  == 2 || num == 4)
 continue;
-}
-else
-{
-_putchar(x + '0');
-}
+_putchar(num + 48);
 }
 _putchar('\n');
 {
